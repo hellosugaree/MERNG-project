0ca6c74edcbe0dbfe91137068b574559254e1627
Timothy Galfin <tnjclgm@gmail.com>
2021-10-19 13:25:36 -0700

    Move File Select into Separate Component

---
+++ client/src/components/FileSelect.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+import "../App.css";
+
+const FileSelect = (props) => {
+  const { handleFileSelect, handleAccept, handleCancel, data } = props;
+
+  return !data ? (
+    <label className="file-input-label">
+      New Profile Photo
+      <input
+        onChange={e => handleFileSelect(e.target.files[0])}
+        type="file"
+        name="images"
+        accept="image/*"
+        style={{ display: "none" }}
+      />
+    </label>
+  ) : (
+    <div>
+      <Button
+        onClick={handleAccept}
+        inline
+        color="teal"
+        type="button"
+        style={{ fontSize: 20, padding: 10 }}
+      >
+        Accept
+      </Button>
+      <Button
+        onClick={handleCancel}
+        inline
+        color="red"
+        type="button"
+        style={{ fontSize: 20, padding: 10 }}
+      >
+        Cancel
+      </Button>
+    </div>
+  );
+};
+
+export default FileSelect;
